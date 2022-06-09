5af182b7be72f85be50ebdba27e777268ec4b931
KiTaeUk <ktw1421@naver.com>
2022-06-09 19:15:14 +0900

    array part1 commit

---
+++ CH2/list4.py
@@ -0,0 +1,9 @@
+#리스트의 모든 원소를 스캔하기(인덱스 값을 사용하지 않음)
+
+x = ['John', 'George', 'Paul', 'Ringo']
+
+for i in x:
+    print(i)
+    
+    
+# 문자열, 리스트, 튜플, 집합, 딕셔너리 등 자료형 객체는 모두 이터러블(iterable):반복가능, iterator(반복자)
