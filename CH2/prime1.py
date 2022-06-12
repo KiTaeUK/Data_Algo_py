1a2f948ebd51ec7a32ebcc09bc9827369a032f0f
KiTaeUk <ktw1421@naver.com>
2022-06-12 23:29:06 +0900

    array part final commit

--- CH2/prime1.py
+++ CH2/prime1.py
@@ -5,9 +5,9 @@ counter = 0
 for n in range(2, 1001):
     for i in range(2,n):
         counter += 1
-        if n%i == 0:
-            break
-    else:
+        if n%i == 0:    # 나누어 떨어지면 소수가 아님
+            break       # 반복은 더 이상 불필요하여 중단
+    else:               # 끝까지 나누어 떨어지지 않으면 다음을 수행
         print(n)
         
 print(f'나눗셈을 실행한 횟수: {counter}')
